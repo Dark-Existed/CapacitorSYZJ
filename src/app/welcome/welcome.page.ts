0a0dc63d6b560600209ba5c20c6b013dcfebed13
Dark-Existed <c2572388311@gmail.com>
2020-10-30 15:39:51 +0800

    feat: finish welcome page

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WelcomePage implements OnInit {
 
+  showSkip = true;
+
+  @ViewChild('slides', {static: false}) slides: IonSlides;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSlideWillChange(event) {
+    event.target.isEnd().then((end) => {
+      this.showSkip = !end;
+    });
   }
 
 }
