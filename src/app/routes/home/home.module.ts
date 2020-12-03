1c3010f276734ad16909fe6ad4e91022b06a6109
Dark-Existed <c2572388311@gmail.com>
2020-12-03 21:28:50 +0800

    change: copyright style

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -7,12 +7,11 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     HomePageRoutingModule
   ],
   declarations: [HomePage]
