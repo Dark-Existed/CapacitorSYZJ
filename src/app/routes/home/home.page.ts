b2a292c10b7d686700649ebfaecf184b84f37731
Dark-Existed <c2572388311@gmail.com>
2020-12-03 20:13:01 +0800

    feat: add statistics widget

--- src/app/routes/home/home.page.ts
+++ src/app/routes/home/home.page.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  private sales = [
+    { title: '今日', content: '比昨天', previous: this.getRandomNum(), current: this.getRandomNum() },
+    { title: '七天', content: '比同期', previous: this.getRandomNum(), current: this.getRandomNum() },
+    { title: '本月', content: '比同期', previous: this.getRandomNum(), current: this.getRandomNum() },
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getRandomNum(): number {
+    return Math.random() * 10;
+  }
+
+  getArrowStatus(current: number, previous: number): number {
+    const result = current - previous;
+    if (result > 0) {
+      return 1;
+    } else if (result === 0) {
+      return 0;
+    } else {
+      return -1;
+    }
   }
 
 }
