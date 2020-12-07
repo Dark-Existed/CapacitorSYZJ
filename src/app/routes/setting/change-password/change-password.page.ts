e6f01813e1953847cd42e319397587ebe12b6020
Dark-Existed <c2572388311@gmail.com>
2020-12-07 16:24:41 +0800

    feat: add change password

--- src/app/routes/setting/change-password/change-password.page.ts
+++ src/app/routes/setting/change-password/change-password.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IonRouterOutlet, ToastController } from '@ionic/angular';
+import { PassportServiceService } from 'src/app/shared/services/passport-service.service';
 
 @Component({
   selector: 'app-change-password',
@@ -13,9 +16,30 @@ export class ChangePasswordPage implements OnInit {
     confirmPassword: '',
   };
 
-  constructor() { }
+  constructor(
+    private outlet: IonRouterOutlet,
+    private toastController: ToastController,
+    private passportService: PassportServiceService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  async onSubmitPassword(form: NgForm) {
+    if (form.valid) {
+      const toast = await this.toastController.create({ duration: 2000 });
+      const currentUser = this.passportService.getCueerntUser();
+      const user = this.passportService.getUser(currentUser.id);
+      if (this.passportService.validatePassword(this.passwordConfig.oldPassword, user.passwordToken)) {
+        this.passportService.updateUserPasswordToken(this.passwordConfig.newPassword, user.id);
+        toast.message = '密码修改成功';
+        toast.present();
+        this.outlet.pop(1);
+      } else {
+        toast.message = '旧密码错误';
+        toast.present();
+      }
+    }
   }
 
 }
