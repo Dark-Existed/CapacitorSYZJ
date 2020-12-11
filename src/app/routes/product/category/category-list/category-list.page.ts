e4fc6764c9ec5fc02d8e8e411c44e42ccfb315cc
Dark-Existed <c2572388311@gmail.com>
2020-12-11 20:39:44 +0800

    feat: add category-list refresh data

--- src/app/routes/product/category/category-list/category-list.page.ts
+++ src/app/routes/product/category/category-list/category-list.page.ts
@@ -1,79 +1,91 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { ActionSheetController } from '@ionic/angular';
 import { Category } from '../category';
 import { CategoryService } from '../category.service';
 
 @Component({
-    selector: 'app-category-list',
-    templateUrl: './category-list.page.html',
-    styleUrls: ['./category-list.page.scss'],
+  selector: 'app-category-list',
+  templateUrl: './category-list.page.html',
+  styleUrls: ['./category-list.page.scss'],
 })
 export class CategoryListPage implements OnInit {
 
-    private categories: Category[];
-    private activateCategory: Category;
-    private activateSubCategories: Category[];
-    private activateSubCategory: Category;
+  private categories: Category[];
+  private activateCategory: Category;
+  private activateSubCategories: Category[];
+  private activateSubCategory: Category;
 
-    constructor(
-        private categoryService: CategoryService,
-        private actionSheetController: ActionSheetController
-    ) {
-        categoryService.getAll().then((data) => {
-            this.categories = data.result;
-            if (this.categories) {
-                this.activateCategory = this.categories[0];
-                this.activateSubCategories = this.activateCategory.children;
-            }
-        });
-    }
-
-    ngOnInit() {
-    }
-
-    onSelectedCategory(category: Category) {
-        this.activateCategory = category;
-        this.activateSubCategories = this.activateCategory.children;
-    }
-
-    onSelectedSubCategory(category: Category) {
-
-    }
-
-    async onPresentActionSheet() {
-        const actionSheet = await this.actionSheetController.create({
-            header: '选择您的操作',
-            buttons: [
-                {
-                    text: '新增小分类',
-                    role: 'destructive',
-                    handler: () => {
-                        console.log('Destructive clicked');
-                    }
-                },
-                {
-                    text: '编辑分类',
-                    handler: () => { console.log('Archive clicked'); }
-                },
-                {
-                    text: '取消',
-                    role: 'cancel',
-                    handler: () => { console.log('Cancel clicked'); }
-                }
-            ]
-        });
-        await actionSheet.present();
-    }
-
-    goToAddCategory() {
-
-    }
-
-    getItemColor(id: number): string {
-        if (id === this.activateCategory.id) {
-            return '';
-        } else {
-            return 'light';
+  constructor(
+    private router: Router,
+    private categoryService: CategoryService,
+    private actionSheetController: ActionSheetController
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/category/category-list') {
+          this.updateData();
         }
+      }
+    });
+  }
+
+  updateData() {
+    this.categoryService.getAll().then((data) => {
+      this.categories = data.result;
+      if (this.categories) {
+        this.activateCategory = this.categories[0];
+        this.activateSubCategories = this.activateCategory.children;
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSelectedCategory(category: Category) {
+    this.activateCategory = category;
+    this.activateSubCategories = this.activateCategory.children;
+  }
+
+  onSelectedSubCategory(category: Category) {
+
+  }
+
+  async onPresentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: '选择您的操作',
+      buttons: [
+        {
+          text: '新增小分类',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },
+        {
+          text: '编辑分类',
+          handler: () => { console.log('Archive clicked'); }
+        },
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: () => { console.log('Cancel clicked'); }
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+
+  goToAddCategory() {
+
+  }
+
+  getItemColor(id: number): string {
+    if (id === this.activateCategory.id) {
+      return '';
+    } else {
+      return 'light';
     }
+  }
 }
