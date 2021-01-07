f938be31a234b18bd1c2a3e02ec63a9596e42a53
Dark-Existed <c2572388311@gmail.com>
2021-01-08 00:02:13 +0800

    feat: add bisic product-stock page

--- src/app/routes/product/product-stock/product-stock.page.ts
+++ src/app/routes/product/product-stock/product-stock.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-stock',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductStockPage implements OnInit {
 
-  constructor() { }
+  private increOrDecre = 'IncreaseStorage';
+  private product: Product;
+  private num: number;
+  private remark: string;
+  private inputDescribe = {
+    IncreaseStorage: '入库数量',
+    DecreaseStorage: '出库数量',
+  };
+  private buttonDescribe = {
+    IncreaseStorage: '确定入库',
+    DecreaseStorage: '确定出库',
+  };
+
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService,
+    private alertController: AlertController,
+    private toastController: ToastController,
+  ) {
+    this.activatedRoute.queryParams.subscribe(queryParams => {
+      this.product = this.productService.getProductByBarcode(queryParams.barcode);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  async onConfirm() {
+
   }
 
 }
